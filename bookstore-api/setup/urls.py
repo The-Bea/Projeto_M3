f0b8f03b49d855246759d70de96df9b9fe4cd6af
Beatriz <beaa.devops@gmail.com>
2026-05-25 15:04:03 -0300

    paginacao com django

--- bookstore-api/setup/urls.py
+++ bookstore-api/setup/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from bookstore.views import home 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('bookstore.urls')),
+    path('', home),
 ]
