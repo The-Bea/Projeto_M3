b21851f03845a282c5cb5b48e8d20ff64bf40b81
Beatriz <beaa.devops@gmail.com>
2026-05-25 14:28:05 -0300

    implementa serializers, factories e testes

--- bookstore-api/bookstore/tests.py
+++ bookstore-api/bookstore/tests.py
@@ -9,3 +9,16 @@ class BookSerializerTest(TestCase):
         data = serializer.data
 
         self.assertEqual(set(data.keys()), set(['id', 'title', 'author', 'price', 'edition']))
+
+        def test_serializer_with_invalid_data(self):
+
+            invalid_data = {
+                'title': 'Livro Teste',
+                'author': 'Autor Teste',
+                'price': 'valor_errado', 
+                'edition': 1
+            }
+            serializer = BookSerializer(data=invalid_data)
+
+            self.assertFalse(serializer.is_valid())
+            self.assertIn('price', serializer.errors)
