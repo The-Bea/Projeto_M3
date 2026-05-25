6e6b04d6d26f8dcd34b8f7b359ba8052596894db
Beatriz <beaa.devops@gmail.com>
2026-05-25 12:08:18 -0300

    configurando o django models

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    publicado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.titulo
