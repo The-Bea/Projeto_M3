d8694c0ccfd2d0433b3e4636ec0314c95716f23e
Beatriz <beaa.devops@gmail.com>
2026-05-25 12:39:05 -0300

    criação de templates e renderização de posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def home_view(request):
-    return HttpResponse("Hello World")
+    posts = Post.objects.filter(publicado=True).order_by('-data_criacao')
+    
+
+    return render(request, 'blog/index.html', {'posts': posts})
+
+def post_detail_view(request, pk):
+
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail.html', {'post': post})
