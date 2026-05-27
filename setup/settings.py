28e1905f39fe6c05f42845ef6aa0b3b277c0561e
Beatriz <beaa.devops@gmail.com>
2026-05-26 21:08:55 -0300

    adicionado docker-compose, postgres e aplicadas as migrações dos apps

--- setup/settings.py
+++ setup/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': os.environ.get('DB_USER', 'bookstore_user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'bookstore_pass'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
